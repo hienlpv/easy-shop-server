3b0ecd6eeab7fbe39987f9e23552b012b65653ad
hienlpv <hienlpv@gmail.com>
2021-08-22 16:57:52 +0700

    22/8/2021

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -6,27 +6,32 @@ function authJwt() {
     return expressJwt({
         secret,
         algorithms: ['HS256'],
-        isRevoked: isRevoked
+        isRevoked: isRevoked,
     }).unless({
         path: [
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            {
+                url: /\/api\/v1\/orders(.*)/,
+                methods: ['GET', 'OPTIONS', 'POST'],
+            },
+            {
+                url: /\/api\/v1\/users(.*)/,
+                methods: ['GET', 'OPTIONS', 'POST'],
+            },
             `${api}/users/login`,
             `${api}/users/register`,
-        ]
-    })
+        ],
+    });
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
-        done(null, true)
+    if (!payload.isAdmin) {
+        done(null, true);
     }
 
     done();
 }
 
-
-
-module.exports = authJwt
+module.exports = authJwt;
