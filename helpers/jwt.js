6d38bb63fc92eb0efc508493689eb110fb1cec80
hienlpv <hienlpv@gmail.com>
2021-08-30 13:03:07 +0700

    30/8/2021

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,7 +14,6 @@ function authJwt() {
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             {
                 url: /\/api\/v1\/orders(.*)/,
-                methods: ['GET', 'OPTIONS', 'POST', 'OPTIONS', 'PUT'],
             },
             {
                 url: /\/api\/v1\/users(.*)/,
